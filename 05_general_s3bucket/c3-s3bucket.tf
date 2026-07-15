981d6d1a3e9615ae611970e0b66311d1de5c3ef8
Dercio Anselmo <dercio@branchdev.io>
2026-07-15 09:35:03 +0400

    Adding seccond bucket

--- 05_general_s3bucket/c3-s3bucket.tf
+++ 05_general_s3bucket/c3-s3bucket.tf
@@ -4,3 +4,10 @@ module "generic_bucket" {
   bucket_name_prefix = var.bucket_name_prefix
   environment_name   = var.environment_name
 }
+
+module "CICD-Security-Scan-report_bucket" {
+  source = "./modules/s3_bucket"
+
+  bucket_name_prefix = "cicd-security-scan-report"
+  environment_name   = var.environment_name
+}
--- 05_general_s3bucket/c4-outputs.tf
+++ 05_general_s3bucket/c4-outputs.tf
@@ -7,3 +7,13 @@ output "generic_bucket_arn" {
   description = "Bucket ARN"
   value       = module.generic_bucket.bucket_arn
 }
+
+output "CICD-Security-Scan-report_bucket_name" {
+  description = "Report Bucket Name"
+  value       = module.CICD-Security-Scan-report_bucket.bucket_name
+}
+
+output "CICD-Security-Scan-report_bucket_arn" {
+  description = "Report Bucket ARN"
+  value       = module.CICD-Security-Scan-report_bucket.bucket_arn
+}
